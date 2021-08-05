936e957d9185dcfed69bb5064a835d34f368d1eb
Chitranshi Srivastava <apigeoneer@gmail.com>
2021-08-05 16:57:07 +0530

    added camera permission check & handled button clicks

--- app/src/main/java/com/gmail/apigeoneer/textrecognitiondemo/ScanningActivity.kt
+++ app/src/main/java/com/gmail/apigeoneer/textrecognitiondemo/ScanningActivity.kt
@@ -1,7 +1,9 @@
 package com.gmail.apigeoneer.textrecognitiondemo
 
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.content.ContextCompat
 import androidx.databinding.DataBindingUtil
 import com.gmail.apigeoneer.textrecognitiondemo.databinding.ActivityScanningBinding
 
@@ -12,5 +14,38 @@ class ScanningActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_scanning)
+
+        binding.apply {
+            takeAPicButton.setOnClickListener {
+                if (checkPermissions()) {
+                    takeAPicture()
+                } else {
+                    requestPermission()
+                }
+            }
+
+            detectNowTextView.setOnClickListener {
+                detectText()
+            }
+        }
+
+    }
+
+    private fun checkPermissions(): Boolean {
+        val cameraPermission = ContextCompat.checkSelfPermission(applicationContext, CAMERA_SERVICE)
+
+        return (cameraPermission == PackageManager.PERMISSION_GRANTED)
+    }
+
+    private fun takeAPicture() {
+
+    }
+
+    private fun requestPermission() {
+
+    }
+
+    private fun detectText() {
+
     }
 }
