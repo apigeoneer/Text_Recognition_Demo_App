b3e3d0fa5597eac1d2de44d65447ef84cf72b064
Chitranshi Srivastava <apigeoneer@gmail.com>
2021-08-05 10:24:21 +0530

    updated main screen ui

--- app/src/main/java/com/gmail/apigeoneer/textrecognitiondemo/MainActivity.kt
+++ app/src/main/java/com/gmail/apigeoneer/textrecognitiondemo/MainActivity.kt
@@ -1,11 +1,28 @@
 package com.gmail.apigeoneer.textrecognitiondemo
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.gmail.apigeoneer.textrecognitiondemo.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        navigateToScanningActivity()
     }
+
+    private fun navigateToScanningActivity() {
+        binding.getstartedButton.setOnClickListener {
+            val intent = Intent(this, ScanningActivity::class.java)
+            startActivity(intent)
+        }
+    }
+
+
 }
